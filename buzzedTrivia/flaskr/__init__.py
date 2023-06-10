a0a12eb4f7a6be81fd17f2e60e22135691a43edc
adambaldwin <abaldwinva98@gmail.com>
2023-06-10 13:35:15 -0400

    Repairing flask app

--- buzzedTrivia/flaskr/__init__.py
+++ buzzedTrivia/flaskr/__init__.py
@@ -1,30 +1,12 @@
-import os
-
 from flask import Flask
 
 
 def create_app():
     # create and configure the app
     app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
     @app.route('/')
     def hello():
         return "Hello"
-
-    from . import db
-    db.init_app(app)
-
-    from . import auth
-    app.register_blueprint(auth.bp)
 
     return app
